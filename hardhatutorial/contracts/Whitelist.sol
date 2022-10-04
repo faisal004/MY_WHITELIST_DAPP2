7a2004a5514c106fae112d6205241efaecedd8ec
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2022-10-04 09:04:52 +0530

    first commit

---
+++ hardhatutorial/contracts/Whitelist.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract Whitelist {
+    uint8 public maxWhitelistedAddresses;
+    mapping(address => bool) public whitelisteAddresses;
+    uint8 public numAddressesWhitelisted;
+    constructor(uint8 _maxWhitelistedAddresses) {
+        maxWhitelistedAddresses =  _maxWhitelistedAddresses;
+    }
+
+    function addAddressToWhitelist()  public {
+        require (!whitelisteAddresses[msg.sender],"Address already registered");
+        require(numAddressesWhitelisted<maxWhitelistedAddresses,"More Address Can Be Registered,Limit Reached");
+        whitelisteAddresses[msg.sender]=true;
+        numAddressesWhitelisted+=1;
+    }
+}
